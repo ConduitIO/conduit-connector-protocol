50ca65965ad46c144d3b6a0109be978b50aa6e8b
Lovro Mažgon <lovro.mazgon@gmail.com>
2022-07-19 19:34:33 +0200

    remove metadata utilities (move to SDK)

--- cpluginv1/metadata.go
+++ cpluginv1/metadata.go
@@ -14,17 +14,6 @@
 
 package cpluginv1
 
-import (
-	"errors"
-	"fmt"
-	"strconv"
-	"time"
-)
-
-var (
-	ErrMetadataValueNotFound = errors.New("metadata value not found")
-)
-
 const (
 	// MetadataCreatedAt is a Record.Metadata key for the time when the record
 	// was created in the 3rd party system. The expected format is a unix
@@ -42,87 +31,3 @@ const (
 	// the plugin that created this record.
 	MetadataConduitPluginVersion = "conduit.plugin.version"
 )
-
-// GetMetadataCreatedAt parses the value for key MetadataCreatedAt as a unix
-// timestamp. If the value does not exist or the value is empty the function
-// returns ErrMetadataValueNotFound. If the value is not a valid unix timestamp
-// in nanoseconds the function returns an error.
-func GetMetadataCreatedAt(metadata map[string]string) (time.Time, error) {
-	raw := metadata[MetadataCreatedAt]
-	if raw == "" {
-		return time.Time{}, fmt.Errorf("failed to get value for %q: %w", MetadataCreatedAt, ErrMetadataValueNotFound)
-	}
-
-	unixNano, err := strconv.ParseInt(raw, 10, 64)
-	if err != nil {
-		return time.Time{}, fmt.Errorf("failed to parse value for %q: %w", MetadataCreatedAt, err)
-	}
-
-	return time.Unix(0, unixNano), nil
-}
-
-// SetMetadataCreatedAt sets the metadata value for key MetadataCreatedAt as a
-// unix timestamp in nanoseconds.
-func SetMetadataCreatedAt(metadata map[string]string, createdAt time.Time) {
-	metadata[MetadataCreatedAt] = strconv.FormatInt(createdAt.UnixNano(), 10)
-}
-
-// GetMetadataReadAt parses the value for key MetadataReadAt as a unix
-// timestamp. If the value does not exist or the value is empty the function
-// returns ErrMetadataValueNotFound. If the value is not a valid unix timestamp
-// in nanoseconds the function returns an error.
-func GetMetadataReadAt(metadata map[string]string) (time.Time, error) {
-	raw := metadata[MetadataReadAt]
-	if raw == "" {
-		return time.Time{}, fmt.Errorf("failed to get value for %q: %w", MetadataReadAt, ErrMetadataValueNotFound)
-	}
-
-	unixNano, err := strconv.ParseInt(raw, 10, 64)
-	if err != nil {
-		return time.Time{}, fmt.Errorf("failed to parse value for %q: %w", MetadataReadAt, err)
-	}
-
-	return time.Unix(0, unixNano), nil
-}
-
-// SetMetadataReadAt sets the metadata value for key MetadataReadAt as a unix
-// timestamp in nanoseconds.
-func SetMetadataReadAt(metadata map[string]string, createdAt time.Time) {
-	metadata[MetadataReadAt] = strconv.FormatInt(createdAt.UnixNano(), 10)
-}
-
-// GetMetadataConduitPluginName returns the value for key
-// MetadataConduitPluginName. If the value is does not exist or is empty the
-// function returns ErrMetadataValueNotFound.
-func GetMetadataConduitPluginName(metadata map[string]string) (string, error) {
-	return getMetadataValue(metadata, MetadataConduitPluginName)
-}
-
-// SetMetadataConduitPluginName sets the metadata value for key
-// MetadataConduitPluginName.
-func SetMetadataConduitPluginName(metadata map[string]string, name string) {
-	metadata[MetadataConduitPluginName] = name
-}
-
-// GetMetadataConduitPluginVersion returns the value for key
-// MetadataConduitPluginVersion. If the value is does not exist or is empty the
-// function returns ErrMetadataValueNotFound.
-func GetMetadataConduitPluginVersion(metadata map[string]string) (string, error) {
-	return getMetadataValue(metadata, MetadataConduitPluginVersion)
-}
-
-// SetMetadataConduitPluginVersion sets the metadata value for key
-// MetadataConduitPluginVersion.
-func SetMetadataConduitPluginVersion(metadata map[string]string, version string) {
-	metadata[MetadataConduitPluginVersion] = version
-}
-
-// getMetadataValue returns the value for a specific key. If the value is does
-// not exist or is empty the function returns ErrMetadataValueNotFound.
-func getMetadataValue(metadata map[string]string, key string) (string, error) {
-	str := metadata[key]
-	if str == "" {
-		return "", fmt.Errorf("failed to get value for %q: %w", key, ErrMetadataValueNotFound)
-	}
-	return str, nil
-}
