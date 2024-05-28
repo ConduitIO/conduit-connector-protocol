1e682f2eafb64b99dbddf4bd6891d4b39d2c28d8
Haris Osmanagic <haris@meroxa.io>
2024-05-28 16:42:40 +0200

    add go types!

---
+++ conduit/schema/v1/schema.go
@@ -0,0 +1,33 @@
+// Copyright © 2024 Meroxa, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1
+
+import (
+	"context"
+	"github.com/conduitio/conduit-commons/schema"
+)
+
+type CreateRequest struct {
+	Bytes []byte
+}
+
+type GetRequest struct {
+	ID string
+}
+
+type SchemaService interface {
+	Create(context.Context, CreateRequest) (schema.Instance, error)
+	Get(context.Context, GetRequest) (schema.Instance, error)
+}
