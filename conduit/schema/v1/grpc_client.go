cff083e3df6b97b8ce1ad9a4dbaccf338927065d
Haris Osmanagic <haris@meroxa.io>
2024-05-29 14:03:21 +0200

    add client

---
+++ conduit/schema/v1/grpc_client.go
@@ -0,0 +1,59 @@
+// Copyright © 2024 Meroxa, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/conduitio/conduit-commons/schema"
+	"github.com/conduitio/conduit-connector-protocol/conduit/schema/v1/fromproto"
+	"github.com/conduitio/conduit-connector-protocol/conduit/schema/v1/toproto"
+	conduitv1 "github.com/conduitio/conduit-connector-protocol/proto/conduit/v1"
+	"google.golang.org/grpc"
+)
+
+type GRPCSchemaClient struct {
+	grpcClient conduitv1.SchemaServiceClient
+}
+
+func NewGRPCSchemaClient() (*GRPCSchemaClient, error) {
+	conn, err := grpc.NewClient("localhost:8085")
+	if err != nil {
+		return nil, fmt.Errorf("failed creating gRPC client: %w", err)
+	}
+
+	return &GRPCSchemaClient{
+		grpcClient: conduitv1.NewSchemaServiceClient(conn),
+	}, nil
+}
+
+func (c *GRPCSchemaClient) Create(ctx context.Context, request CreateRequest) (schema.Instance, error) {
+	resp, err := c.grpcClient.Create(ctx, toproto.CreateSchemaRequest(request))
+	if err != nil {
+		return schema.Instance{}, fmt.Errorf("failed creating schema: %w", err)
+	}
+
+	return fromproto.SchemaInstance(resp.Schema), nil
+}
+
+func (c *GRPCSchemaClient) Get(ctx context.Context, request GetRequest) (schema.Instance, error) {
+	resp, err := c.grpcClient.Get(ctx, toproto.GetSchemaRequest(request.ID))
+	if err != nil {
+		return schema.Instance{}, fmt.Errorf("failed creating schema: %w", err)
+	}
+
+	return fromproto.SchemaInstance(resp.Schema), nil
+}
