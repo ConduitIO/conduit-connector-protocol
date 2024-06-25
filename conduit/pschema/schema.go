da1f64d51c728dbb8ef8325ea90c52249baf8901
Haris Osmanagic <haris@meroxa.io>
2024-06-25 14:39:59 +0200

    typed mock

--- conduit/pschema/schema.go
+++ conduit/pschema/schema.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate mockgen -destination=mock/pschema.go -package=mock -mock_names=Service=Service . Service
+//go:generate mockgen -typed -destination=mock/pschema.go -package=mock -mock_names=Service=Service . Service
 
 package pschema
 
