5c6d68ebc3fd4b4adfff1a2fb40831e0053e5971
Haris Osmanagic <haris@meroxa.io>
2024-07-15 13:35:40 +0200

    add enrich for cfg

--- pconduit/context.go
+++ pconduit/context.go
@@ -14,7 +14,13 @@
 
 package pconduit
 
-import "context"
+import (
+	"context"
+
+	"github.com/conduitio/conduit-connector-protocol/pconnector"
+)
+
+// -- Connector token ----------------------------------------------------------
 
 type connectorTokenCtxKey struct{}
 
@@ -31,4 +37,49 @@ func ConnectorTokenFromContext(ctx context.Context) string {
 		return connectorToken.(string) //nolint:forcetypeassert // only this package can set the value, it has to be a string
 	}
 	return ""
+}
+
+// -- Connector ID ----------------------------------------------------------
+
+type connectorIDCtxKey struct{}
+
+// ContextWithConnectorID wraps ctx and returns a context that contains the connector ID.
+func ContextWithConnectorID(ctx context.Context, connectorID string) context.Context {
+	return context.WithValue(ctx, connectorIDCtxKey{}, connectorID)
+}
+
+// ConnectorIDFromContext fetches the connector ID from the context. If the
+// context does not contain a connector ID it returns an empty string.
+func ConnectorIDFromContext(ctx context.Context) string {
+	connectorID := ctx.Value(connectorIDCtxKey{})
+	if connectorID != nil {
+		return connectorID.(string) //nolint:forcetypeassert // only this package can set the value, it has to be a string
+	}
+	return ""
+}
+
+// -- Connector token ----------------------------------------------------------
+
+type logLevelCtxKey struct{}
+
+// ContextWithLogLevel wraps ctx and returns a context that contains the connector LogLevel.
+func ContextWithLogLevel(ctx context.Context, logLevel pconnector.LogLevel) context.Context {
+	return context.WithValue(ctx, logLevelCtxKey{}, logLevel)
+}
+
+// LogLevelFromContext fetches the connector LogLevel from the context. If the
+// context does not contain a connector LogLevel it returns an empty string.
+func LogLevelFromContext(ctx context.Context) pconnector.LogLevel {
+	logLevel := ctx.Value(logLevelCtxKey{})
+	if logLevel != nil {
+		return logLevel.(pconnector.LogLevel) //nolint:forcetypeassert // only this package can set the value, it has to be a string
+	}
+
+	return pconnector.NoLevel
+}
+
+func Enrich(ctx context.Context, cfg pconnector.PluginConfig) {
+	ContextWithConnectorToken(ctx, cfg.Token)
+	ContextWithConnectorID(ctx, cfg.ConnectorID)
+	ContextWithLogLevel(ctx, cfg.LogLevel)
 }
