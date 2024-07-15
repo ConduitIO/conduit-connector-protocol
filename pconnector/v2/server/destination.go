68a327ee6a4e92a22877f1ce887a4c9b031d774d
Haris Osmanagic <haris@meroxa.io>
2024-07-15 12:49:58 +0200

    linter

--- pconnector/v2/server/destination.go
+++ pconnector/v2/server/destination.go
@@ -16,6 +16,7 @@ package server
 
 import (
 	"context"
+
 	"github.com/conduitio/conduit-connector-protocol/pconnector"
 	"github.com/conduitio/conduit-connector-protocol/pconnector/v2/fromproto"
 	"github.com/conduitio/conduit-connector-protocol/pconnector/v2/toproto"
